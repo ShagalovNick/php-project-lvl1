498d1190a75bd974e25b307967ba08abefbf27c8
ShagalovNick <sekunda-61@yandex.ru>
2022-04-07 15:25:15 +0300

    edit Cli.php, Even.php for optimization

--- src/Cli.php
+++ src/Cli.php
@@ -12,3 +12,30 @@ function hello()
     line("Hello, %s!", $name);
     return $name;
 }
+
+function ruleLine ()
+{
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+}
+
+function askQuestion($a)
+{
+    line("Question: {$a}");
+    return prompt('Your answer');
+}
+
+function sayCorrect()
+{
+    line('Correct!');
+}
+
+function sayWrong($answer, $rightanswer, $name)
+{
+    line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightanswer}'");
+    line("Let's try again, {$name}!");
+}
+
+function sayGrac($name)
+{
+    line("Congratulations, {$name}!");
+}
--- src/Even.php
+++ src/Even.php
@@ -2,29 +2,25 @@
 
 namespace Brain\Games\Even;
 
-use function cli\line;
-use function cli\prompt;
+use Brain\Games\Cli;
 
 function isEven($name)
 {
-    line('Answer "yes" if the number is even, otherwise answer "no".');
+    Cli\ruleLine();
     $correct = 0;
     while ($correct < 3) { 
         $a = rand(1, 99);
-        line("Question: {$a}");
-        $answer = prompt('Your answer');
+        $answer = Cli\askQuestion($a);
         if (($a % 2 === 0 && $answer === 'yes') || ($a % 2 !== 0 && $answer === 'no')) {
-            line('Correct!');
-            $correct += 1;
-            
+            Cli\sayCorrect();
+            $correct += 1;            
         } else {
             ($a % 2 !== 0) ? $rightanswer = 'no' : $rightanswer = 'yes';
-            line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightanswer}'");
-            line("Let's try again, Bill!");
+            Cli\sayWrong($answer, $rightanswer, $name);
             return;
         }
     }
-    line("Congratulations, {$name}!");
+    Cli\sayGrac($name);
 }
 
 
