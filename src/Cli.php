845665801090db5d452afcc36e1a6dc15bd4107d
ShagalovNick <sekunda-61@yandex.ru>
2022-04-08 21:12:16 +0300

    fix Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -7,12 +7,12 @@ use function cli\prompt;
 
 function hi()
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
 }
 
 function hello()
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     return $name;
